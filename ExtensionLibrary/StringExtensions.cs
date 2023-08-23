1965850c6f9705151b935a3971fcbf623b2e7051
Greg Doud <gpdoud@gmail.com>
2023-08-23 15:52:30 -0400

    Add project files.

---
+++ ExtensionLibrary/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace ExtensionLibrary;
+
+public static class StringExtensions {
+
+    public static int Eval(this int i) {
+        return (i * i) + (3 * i) + 5;
+    }
+
+    public static void Writeline(this string str, string prefix = "", string suffix = "") {
+        Console.WriteLine($"{prefix}{str}{suffix}");
+    }
+
+    public static decimal ToDecimal(this string str) {
+        var s = str;
+        s = s.Trim();
+        if(s[0] == '$') {
+            s = s.Substring(1);
+        }
+        s = s.Replace(",", "");
+        decimal nbr;
+        var rc = decimal.TryParse(s, out nbr);
+        if(!rc) {
+            throw new Exception("string contains nonnumeric characters");
+        }
+        return nbr;
+    }
+
+    public static string AllCaps(this string str) {
+        return str.ToUpper();
+    }
+    public static string AllSmall(this string str) {
+        return str.ToLower();
+    }
+
+}
---
+++ TestExtensionLibrary/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExtensionLibrary;
+
+var s = "   $123,456.78";
+var s1 = "000000000000023455";
+
+var fn = 2;
+
+Console.WriteLine($"fn({fn}) = X^2 + 3X + 5 evaluates to {fn.Eval()}");
+
+s1.Writeline("The value of s1 is ", " pretty cool!!!");
+
+Console.WriteLine($"{s} as a decimal is {s.ToDecimal():C}");
+/*
+
+var s = "kdKjlKLsjLfkd";
+
+var sAllCaps = s.AllCaps();
+var sAllSmall = sAllCaps.AllSmall();
+
+Console.WriteLine($"{s} as all caps is {sAllCaps} and all small is {sAllSmall}");
+*/
